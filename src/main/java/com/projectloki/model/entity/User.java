ae2bcfaaaa2ef9b94a7289e20473fd535aeb1252
LucasCamino05 <lucascamino05@gmail.com>
2025-10-21 16:43:28 -0300

    Dtos Client, ModelMapperConfig. Basic Endpoints

--- src/main/java/com/projectloki/model/entity/User.java
+++ src/main/java/com/projectloki/model/entity/User.java
@@ -1,31 +1,51 @@
 package com.projectloki.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Data
 @ToString
 @EqualsAndHashCode
+@Getter
+@Setter
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "perfil_seq_gen",
+            sequenceName = "perfil_seq_gen",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "perfil_seq_gen")
     private Long id;
 
     private String username;
     private String email;
     private String password;
-    private String name;
-    private String lastName;
+    private String fullName;
     private String DNI;
     private String phoneNumber;
     private String address;
-    private String Nationality;
+    private String nationality;
+    private LocalDate upDate;
 
+    // Constructor sin id
+
+    public User(String username, String email, String password, String fullName, String DNI, String phoneNumber, String address, String nationality) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+        this.DNI = DNI;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.nationality = nationality;
+    }
+
+    public void setUpDate(LocalDate upDate) {
+        this.upDate = upDate;
+    }
 }
